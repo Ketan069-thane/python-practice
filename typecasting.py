a2822b6ee6b821aa94e306cdb67f27ce776816ca
Ketan069-thane <aaravrupare@gmail.com>
2025-12-17 15:49:27 +0530

    Create typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,15 @@
+# Type Casting also known as type conversion, it is process of converting a value from a data type to another data type 
+
+# Code :
+# To print the type of the data type we use :
+#For E.g :
+a = 34
+b = 36.2
+c = "Shubham"
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+      
