213e54c4bf07448e25127d1d47a2f376d96688d6
Ketan069-thane <aaravrupare@gmail.com>
2025-12-17 14:27:43 +0530

    Create data-types.py

---
+++ data-types.py
@@ -0,0 +1,20 @@
+# There are several data types in Python :
+# 1st is Integer Data Type --- which stores the values likes WHOLE NUMBERS ( For E.g-- 10 , -5) it does not store any decimal values
+a = int(34)
+print("The Integer value is : " ,a)
+
+# 2nd is Float Data Types --- which stores the values like Decimal Number , it stores all the decimal values ( for E.g -- [ -5.66 , 965.55 ]
+b = float(89.699)
+print("The Float value is :" ,b)
+
+# 3rd is String Data Types -- which stores all the characters values which are enclosed in Double Quotes for e.g : "Hello"
+name = str("Ketan")
+print("The String Value is:" ,name)
+
+# 4th is Boolean Data Types --- which stores the values of True and False 
+exam = bool("True")
+print("Are you pass in exam" ,exam)
+
+# 5th is Lists Data Types --- which stores mutable values
+list = [1,2,3]
+print("The Sequence is" ,list)
