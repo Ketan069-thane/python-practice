2eabada3048759b051ddd9671fba487054065532
Ketan Yadav <aaravrupare@gmail.com>
2025-12-18 15:01:43 +0530

    Create Assignmentopeartor.py

---
+++ Assignmentopeartor.py
@@ -0,0 +1,68 @@
+# Assignment Operators in Python
+a = 23
+
+# Simple Assignment Operator
+print("Initial value of a is :", a)
+
+# Addition Assignment Operator
+a += 5
+print("After a += 5, value of a is :", a)
+
+# Subtraction Assignment Operator
+a -= 3
+print("After a -= 3, value of a is :", a)
+
+# Multiplication Assignment Operator
+a *= 2
+print("After a *= 2, value of a is :", a)
+
+# Division Assignment Operator
+a /= 4
+print("After a /= 4, value of a is :", a)
+
+# Modulus Assignment Operator
+a %= 3
+print("After a %= 3, value of a is :", a)
+
+# Exponentiation Assignment Operator
+a **= 3
+print("After a **= 3, value of a is :", a)
+
+# Floor Division Assignment Operator
+a //= 5
+print("After a //= 5, value of a is :", a)
+
+#For User Input Case
+print("\nFor User Input Case")
+a = int(input("Enter any Number to get the results:- "))
+
+# Simple Assignment Operator
+print("Initial value of a is :", a)
+
+# Addition Assignment Operator
+a += 5
+print("After a += 5, value of a is :", a)
+
+# Subtraction Assignment Operator
+a -= 3
+print("After a -= 3, value of a is :", a)
+
+# Multiplication Assignment Operator
+a *= 2
+print("After a *= 2, value of a is :", a)
+
+# Division Assignment Operator
+a /= 4
+print("After a /= 4, value of a is :", a)
+
+# Modulus Assignment Operator
+a %= 3
+print("After a %= 3, value of a is :", a)
+
+# Exponentiation Assignment Operator
+a **= 3
+print("After a **= 3, value of a is :", a)
+
+# Floor Division Assignment Operator
+a //= 5
+print("After a //= 5, value of a is :", a)
