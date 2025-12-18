d0e4dd05f23c77442f1c0950655b86318fc9ff95
Ketan Yadav <aaravrupare@gmail.com>
2025-12-18 14:12:13 +0530

    Create userinput.py

---
+++ userinput.py
@@ -0,0 +1,7 @@
+# In this section we will see how to take input from user
+a = input("Enter Your Name :-")
+print("Your Name is : " ,a)
+
+# Along with Data Type
+b = int(input("Enter the number :-"))
+print("The Entered Value is: " ,b)
