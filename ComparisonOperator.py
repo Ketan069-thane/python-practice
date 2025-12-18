3052ccb9a4966b2bf4fea3149d5ec573dba34deb
Ketan Yadav <aaravrupare@gmail.com>
2025-12-18 14:34:37 +0530

    Create ComparisonOperator.py

---
+++ ComparisonOperator.py
@@ -0,0 +1,37 @@
+#Comparison Operator
+# 1st is Greater Than and Greater than equal Operator
+x = 23
+y = 33
+#It prints Boolean Values like True and False
+print("Is X is Greater than Y: ", x >= y)
+
+# 2nd is Smaller and Smaller than equal operator
+a = 66
+b = 33
+print("Is A is smaller than or equal to B" , a <= b)
+
+# 3rd is Equal to operator
+c = 99
+d = 99
+print("Is C is equal to D", c == d)
+
+# 4th is Not Equal to Operator
+e = 56
+f = 55
+print("Is E and F are not equal:-" , e != f)
+
+#For User Input
+x = int(input("Enter a Number:-"))
+y = int(input("Enter a Number:-"))
+#It prints Boolean Values like True and False
+print("Is X is Greater than Y: ", x >= y)
+
+# 2nd is Smaller and Smaller than equal operator
+print("Is A is smaller than or equal to B" , x <= y)
+
+# 3rd is Equal to operator
+print("Is C is equal to D", x == y)
+
+# 4th is Not Equal to Operator
+
+print("Is E and F are not equal:-" , x != y)
