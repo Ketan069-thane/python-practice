162ad86cd2edb360e09b93ce085d57d16a56d506
Ketan Yadav <aaravrupare@gmail.com>
2025-12-18 14:22:01 +0530

    Create arithemeticoperator.py

---
+++ arithemeticoperator.py
@@ -0,0 +1,16 @@
+a = int(input("Enter 1st Number :-"))
+b = int(input("Enter 2nd Number :-"))
+# For Addition Operator
+print("The addition of the 2 numbers are :- " , a + b)
+#For Substraction Operator
+print("The substraction of the 2 numbers are :- ", a - b)
+#For Multiplication Operator
+print("The product of the 2 numbers are :- ", a * b)
+#For Division Operator
+print("The division of the 2 numbers are :-", a / b)
+#For Float Division (//) Opeartor
+print("The Float Division of the 2 numbers are :- ", a // b)
+#For Modulus Operator
+print("The Modulus of the 2 numbers are :-", a % b)
+#For Float Multiplication Operator
+print("The Float Mutliplication of the 2 numbers are :-" , a ** b)
