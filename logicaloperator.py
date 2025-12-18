38ba36c9526b023a9bc2d21b936ad44d57240090
Ketan Yadav <aaravrupare@gmail.com>
2025-12-18 14:49:19 +0530

    Create logicaloperator.py

---
+++ logicaloperator.py
@@ -0,0 +1,41 @@
+#Comparison Operator
+# 1st is Greater Than and Greater than equal Operator
+x = True
+y = False
+# 1st For AND Operator
+print("\nFor AND Operaotors")
+print("The Value for x and x is :", x and x)
+print("The Value for x and y is :", x and y)
+print("The Value for y and y is :", y and y)
+print("The Vlaue for y and x is :", y and y) 
+# 2nd For OR Operator
+print("\nFor OR operators")
+print("The Value for x or x is :", x or x)
+print("The Value for x or y is :", x or x)
+print("The Value for y or y is :", x or x)
+print("The Value for y or x is :", x or x)
+# 3rd For NOT Operator
+print("\nFor NOT Operator")
+print("The Value for not x is : ", not x)
+print("The Value for not y is : ", not y)
+
+# For User Input Case 
+print("\nFor User Input Case:")
+a = bool(input("Enter the boolean True/False-Value:-"))
+b = bool(input("Enter the boolean True/False-Value:-"))
+# 1st For AND Operator
+print("\nFor AND Operaotors")
+print("The Value for a and a is :", a and a)
+print("The Value for a and b is :", a and b)
+print("The Value for b and b is :", b and b)
+print("The Vlaue for b and a is :", b and a)
+# 2nd For OR Operator
+print("\nFor OR operators")
+print("The Value for a or a is :", a or a)
+print("The Value for a or b is :", a or b)
+print("The Value for b or b is :", b or b)
+print("The Value for b or a is :", b or a)
+# 3rd For NOT Operator
+print("\nFor NOT Operator")
+print("The Value for not a is : ", not a)
+print("The Value for not b is : ", not b)
