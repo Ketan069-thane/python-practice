58bca11d254e5ad4615b345219ea057b804a4f86
Ketan Yadav <aaravrupare@gmail.com>
2025-12-18 15:09:07 +0530

    Create conditionas.py

---
+++ conditionas.py
@@ -0,0 +1,6 @@
+age = float(input("Enter your age: "))
+if age <= 18:
+    print("You are a minor because your age is less than 18.")
+
+else:
+    print("You are an adult because your age is greater than 18.")
