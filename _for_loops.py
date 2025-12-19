b8ff2eb738c74d6cd12c7f15fd40e392a23fd95a
Ketan Yadav <aaravrupare@gmail.com>
2025-12-19 14:33:47 +0530

    Add multiplication table using for loops

---
+++ _for_loops.py
@@ -0,0 +1,8 @@
+# We will start with FOR Loop :
+for i in range (1 , 16)
+print("5 x" , i , "=" , 5 * i)
+
+# For User Input Case
+k = int(input("Enter any number to get is table from 1 to 10:\n"))
+for j in range (1 , 21):
+    print(k ,"x", j , "=", k * j)
