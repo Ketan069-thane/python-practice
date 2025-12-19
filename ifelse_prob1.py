10e361f1a07fdda9c3b58876cd2071e4fc903c86
Ketan Yadav <aaravrupare@gmail.com>
2025-12-19 23:54:22 +0530

    Implement number sign checker
    The program checks if a number is positive, negative, or zero.

---
+++ ifelse_prob1.py
@@ -0,0 +1,12 @@
+#Write a program that asks the user for a number and prints whether it is positive, negative, or zero.
+i = int(input("Enter a number: \n"))
+print(num)
+
+if(i<0):
+    print("Number is negative")
+    
+elif(i>0):
+    print("Number is positive")
+
+else:
+    print("Number is zero")
