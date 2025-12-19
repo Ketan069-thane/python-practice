6f4d1322e1d07f2081253b4be35e80b35ef1a27d
Ketan Yadav <aaravrupare@gmail.com>
2025-12-20 00:05:59 +0530

    Implement match statement for days of the week

---
+++ match-prob1.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number:\n"))
+
+match num:
+    case 1:
+        print("Its Sunday")
+    case 2:
+        print("Its Monday")
+    case 3:
+        print("Its Tuesday")
+    case 4:
+        print("Its Wednesday")
+    case 5:
+        print("Its Thursday")
+    case 6:
+        print("Its Friday")
+    case 7:
+        print("Its Saturday")
