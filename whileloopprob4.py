e36e7417fccdda51507f52505f1c51d3468a5c85
Ketan Yadav <aaravrupare@gmail.com>
2025-12-20 00:38:16 +0530

    Implement password verification loop

---
+++ whileloopprob4.py
@@ -0,0 +1,7 @@
+password = "Ketan"
+entered_pass = input("Enter the Password: \n")
+
+while (entered_pass != password):
+    entered_pass = input("Wrong Password, Try again :\n")
+
+print("Success ! Welcome")
