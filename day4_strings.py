b27a320ebaa8a1738def32f89b052b82572463d5
Ketan Yadav <aaravrupare@gmail.com>
2025-12-20 17:18:51 +0530

    Add examples of string declarations in Python

---
+++ day4_strings.py
@@ -0,0 +1,16 @@
+#For double quotes
+name = "Ketan"
+print(name)
+
+#For single quotes
+name1 = 'Ketan'
+print(name1)
+
+#For triple quotes
+name2 = '''Ketan is
+a
+developer
+and
+he works at
+Google'''
+print(name2)
