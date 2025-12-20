79612b66a924672b8f51cbd7ae1043e1c117f069
Ketan Yadav <aaravrupare@gmail.com>
2025-12-20 17:27:01 +0530

    Add string slicing examples in day4_slicing.py

---
+++ day4_slicing.py
@@ -0,0 +1,8 @@
+name ="Ketan"
+print(name[0:3]) # Accessing first three characters
+print(name[2:5]) # Accessing characters from index 2 to 4
+print(name[:4])  # Accessing first four characters  
+print(name[2:])  # Accessing characters from index 2 to end
+print(name[-1])  # Accessing last character
+print(name[-3:]) # Accessing last three characters
+print(name[::-1]) # Reversing the string
