a69b0c246556857713381a8dc16efc4aec256a53
Ketan Yadav <aaravrupare@gmail.com>
2025-12-20 17:19:23 +0530

    Add string indexing and slicing examples

---
+++ day4_index.py
@@ -0,0 +1,15 @@
+name ="Ketan"
+
+print(name[0]) # Accessing first character
+print(name[1]) # Accessing second character
+print(name[2]) # Accessing third character
+print(name[3]) # Accessing fourth character
+print(name[4]) # Accessing fifth character
+#print(name[5]) # Accessing sixth character
+print(name[0:3]) # Accessing first three characters
+print(name[2:5]) # Accessing characters from index 2 to 4
+print(name[:4])  # Accessing first four characters  
+print(name[2:])  # Accessing characters from index 2 to end
+print(name[-1])  # Accessing last character
+print(name[-3:]) # Accessing last three characters
+print(name[::-1]) # Reversing the string
