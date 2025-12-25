5247146ca285e88c84e31f2867bfbbb130bcb153
Ketan Yadav <aaravrupare@gmail.com>
2025-12-25 11:55:14 +0530

    Prompt for name and display its length
    Add functionality to prompt user for name and print its length

---
+++ string_method.py
@@ -0,0 +1,7 @@
+name = str(input("Enter your name: "))  # Prompt user for input
+
+a = len(name)
+print(a)  # Output: 5
+
+#Blank character are also counted in this
+#Len is the function to find the length of string
