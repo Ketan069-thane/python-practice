fc2ce90d122b0ddcb453d35319049bd4e0cc8489
Ketan Yadav <aaravrupare@gmail.com>
2025-12-25 11:56:47 +0530

    Implement whitespace trimming in removewhitespace.py
    Add functions to trim whitespace from a string

---
+++ removewhitespace.py
@@ -0,0 +1,7 @@
+text = "  hello world  "
+
+print(text.strip())  # It trims whitespace from both ends
+
+print(text.lstrip())  # It trims leading whitespace
+
+print(text.rstrip())  # It trims trailing whitespace
