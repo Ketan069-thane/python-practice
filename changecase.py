6e8255f051305ca12373b192b20c7d7546b0fb92
Ketan Yadav <aaravrupare@gmail.com>
2025-12-25 11:55:47 +0530

    Create changecase

---
+++ changecase.py
@@ -0,0 +1,14 @@
+# Changing case of the string and finding the lemgth of stringth of string
+s = "Hello World"
+
+a = len(s)
+print(a)  # Output: 11
+print(s.upper()) # It converts all the characters to uppercase
+
+ # Output: "HELLO WORLD" ----This is called as immutability
+
+print(s.lower())  #It converts all the characters to lowercase
+
+print(s.capitalize())  #It converts first character to uppercase and rest to lowercase
+
+print(s.title())  # It converts first character of each word to uppercase
