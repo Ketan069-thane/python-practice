8a052e539d66eae8108a7afda25539ec6b76a9fc
Ketan Yadav <aaravrupare@gmail.com>
2025-12-25 11:57:48 +0530

    Add string check functions for alphanumeric validation

---
+++ stringcheck.py
@@ -0,0 +1,9 @@
+text = "Python123"
+
+print(text.isalnum())  # It checks if all characters are alphanumeric
+
+print(text.isalpha())  # It checks if all characters are alphabetic
+
+print(text.isdigit())  # It checks if all characters are digits
+
+print(text.isspace())  # It checks if all characters are whitespace
