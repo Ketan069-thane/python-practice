a44ba2f39ca4dbe4d7410c53c56800d1fbc83884
Ketan Yadav <aaravrupare@gmail.com>
2025-12-27 00:00:10 +0530

    Revision session
    Added explanations and examples for various Python data types including Integer, Float, String, and Boolean.

---
+++ revisiondatatypes.py
@@ -0,0 +1,30 @@
+#There are several Data types in python
+#1st is Integer Data Type
+#All the data present in the string will be integer for e.g [-3,-2,-1,0,1,2,3] and so on no other values like decimal and alphabets are involved in this
+
+#Integer Data Type
+a = 32
+print(a)
+print(type(a))
+
+#type function describes the exact data type 
+
+#Float Data Type
+# In this data type only decimal values are present no , integer values or characters
+
+b = -15.99
+print(b)
+print(type(b))
+
+#String Data type
+# String data type is used to store the characters values like all the alphabets in ""
+
+c = "Ketan"
+print(c)
+print(type(c))
+
+# 4th is Boolean Data Types --- which stores the values of True and False 
+exam = bool("True")
+print("Are you pass in exam" ,exam)
+print(type(exam))
+
