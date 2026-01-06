f184e5d1c3af54ff521613b321b1a784417dfaa4
Ketan Yadav <aaravrupare@gmail.com>
2026-01-06 15:15:52 +0530

    Add script to calculate average of three numbers

---
+++ 01_withoutfunction.py
@@ -0,0 +1,7 @@
+# calculating average of three numbers without function
+a = int( input("Enter first number: "))
+b = int( input("Enter second number: "))
+c = int( input("Enter third number: "))
+
+average = (a + b + c) / 3
+print("The average is:", average)
